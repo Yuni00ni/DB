298a57cd62931e7fedc63eb1a50f754b5a98e036
uni <moonrit001@gmail.com>
2025-06-23 12:30:04 +0900

    주석 추가

--- 02. where.sql
+++ 02. where.sql
@@ -94,7 +94,7 @@ select first_name, salary, department_id from employees where department_id = 30
 select * from employees where department_id = 50 and job_id = 'ST_MAN';
 
 -- 문제2) 부서가 100이거나 급여가 7000 미만인 직원의 직원id, 이름, 부서정보, 급여정보를 조회하시오.
-select employee_id, first_name, department_id, salary from employees where department_id = 100 or salary < 7000;
+select employee_id, first_name, department_id, salary from employees where department_id = 100 or  salary < 7000;
 
 -- 문제3) 직책이 st_man이고 급여가 7000미만인 직원의 이름, 직책, 급여 정보를 조회하시오.
 select first_name, job_id, salary from employees where job_id = 'ST_MAN' and salary < 7000;
@@ -104,3 +104,4 @@ select first_name, department_id, salary from employees where department_id = 50
 
 -- 문제5) 부서가 100이거나 90인 직원 중에 직원id가 101인 직원의 직원id, 이름, 급여, 연봉 정보를 조회하시오.
 select department_id, employee_id, first_name, salary, salary * 12 as "연봉" from employees where (department_id = 100 or department_id = 90) and employee_id = 101;
+-- and가 연산 순서가 or 보다 높은건 연산이 먼저 처리되는 것. 조건의 우선순위가 높은 것이 아님. 혼동하지 말 것.
