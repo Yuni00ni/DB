093afe40a4e41039237a7e5ce3030815e6bb13c1
uni <moonrit001@gmail.com>
2025-06-25 12:11:14 +0900

    group by 문제풀이까지

--- 04. group by절.sql
+++ 04. group by절.sql
@@ -48,3 +48,51 @@ select max(salary), min(salary) from employees;
 
 select round(avg(department_id), 1) from employees;
 
+select * from 수강생정보;
+
+select * from 성적표;
+
+-- group by절이란?
+-- 특정 컬럼을 기준으로 그룹화(묶는다)
+
+-- sql 작성 순서
+-- select > from > where > group by > having > order by;
+
+-- *** sql 실행 순서 ***
+-- from > where > group by > having > select > order by;
+
+-- [ group by절 사용 방법 ]
+-- select : 조회하고자 하는 "컬럼의 정보"
+-- from : 데이터를 가져올 "테이블의 정보"
+-- where : 원하는 행(데이터)를 선별하기 위한 "조건식"
+-- group by : 특정 컬럼을 기준으로 "그룹화"
+
+-- 수강생정보테이블에서 소속반 별로 반별 인원수를 집계를 통해 구해보자.
+select 소속반, count(학생id) as "반별 인원수" from 수강생정보 group by 소속반;
+
+select * from 수강생정보;
+
+-- group by란 통계적인 자료를 가지고 분석을 하기 위해 사용하는 것
+-- 특정 컬럼을 적용시켜서 그룹화를 하고 통계적 자료를 분석하기 위해 사용한다.
+
+-- [ group by절을 사용할 때 유의해야할 점 ]
+-- 1. group by절을 사용하는 순간 행의 갯수가 감소됨
+--> group by절보다 늦게 실행되는 절인 select절에는 출력할 수 있는 행이 제한이 걸리게 됨
+-- 2. 다만 집계함수로 처리하는 컬럼에 한에서는 사용이 가능하다.
+
+select * from 수강생정보;
+
+-- 수강생정보 테이블에서 소속반 별로 그룹화 하여 반별 인원수를 집계하겠다. >> group by를 사용할 컬럼을 확실하게 구분지을 수 있어야 함
+
+select 소속반, count(*) as "반별 인원수" from 수강생정보 group by 소속반;
+
+-- 성적표 테이블에서 과목별로 최고 성적과 최저 성적을 출력
+select 과목, max(성적) as "최고 성적", min(성적) as "최저 성적" from 성적표 group by 과목;
+
+-- 직원테이블에서 부서별 직원의 급여 총 합계를 구하시오.
+select department_id, sum(salary) as "sum" from employees group by department_id;
+
+-- 직원데이블에서 직업별 직원의 평균 급여를 구하시오.
+select job_id, round(avg(salary), 1) as "avg" from employees where department_id is not null group by job_id;
+
+
